dc75e29b1c908e7c753efd06554313def190cb14
yigitsk <yigitskucuksoz@gmail.com>
2020-07-06 00:57:13 +0300

    +Visual changes

---
+++ src/Presentation/Nop.Web/Components/HeaderUpperLinks.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class HeaderUpperLinksViewComponent : NopViewComponent
+    {
+        private readonly ICommonModelFactory _commonModelFactory;
+
+        public HeaderUpperLinksViewComponent(ICommonModelFactory commonModelFactory)
+        {
+            _commonModelFactory = commonModelFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _commonModelFactory.PrepareHeaderLinksModel();
+            return View(model);
+        }
+    }
+}
